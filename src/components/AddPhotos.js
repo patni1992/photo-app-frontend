383e0f111ad57e5653883da9cc0a521c1c0d9882
Patrik Nilsson <pn222en@student.lnu.se>
2018-04-01 22:26:29 +0700

    Add axios, send form request on submit

--- src/components/AddPhotos.js
+++ src/components/AddPhotos.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
 const Form = styled.form`
 	position: relative;
@@ -49,20 +50,36 @@ class AddPhotos extends Component {
 		};
 	}
 	onDrop = (acceptedFiles, rejectedFiles) => {
-		console.log(acceptedFiles);
-		console.log(rejectedFiles);
 		this.setState({
 			img: acceptedFiles[0]
 		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		const bodyFormData = new FormData();
+		bodyFormData.append('description', event.target.elements.description.value);
+		bodyFormData.append('tags', event.target.elements.tags.value);
+		bodyFormData.append('image', this.state.img);
+
+		axios
+			.post('http://localhost:1337/images', bodyFormData)
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 	};
 
 	render() {
 		return (
 			<div>
 				<h2>Add a photo</h2>
-				<Form action="">
-					<input placeholder="Description" type="text" />
-					<input placeholder="Tags (seperate with comma)" type="text" />
+				<Form onSubmit={this.handleSubmit}>
+					<input name="description" placeholder="Description" type="text" />
+					<input name="tags" placeholder="Tags (seperate with comma)" type="text" />
 					<Dropzone
 						style={{ background: 'white', border: '3px black dotted', padding: '80px', margin: '0 0 15px' }}
 						onDrop={this.onDrop}
