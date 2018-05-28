536652247e03b882b90942557e813455850881c5
Patrik Nilsson <pn222en@student.lnu.se>
2018-05-28 13:50:15 +0200

    Change signup request

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,7 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
+
+import { SET_ERRORS } from './types';
 
 export function signupRequest(userData) {
-  return dispatch => {
-    return axios.post("/users", userData);
-  };
+	return dispatch => {
+		return axios
+			.post('/users', userData)
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => {
+				console.log(err.response.data);
+				dispatch({
+					type: SET_ERRORS,
+					payload: err.response.data
+				});
+			});
+	};
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import imageReducer from './imageReducer';
 import activeEditImageReducer from './activeEditImageReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
 	images: imageReducer,
-	activeEditImage: activeEditImageReducer
+	activeEditImage: activeEditImageReducer,
+	errors: errorReducer
 });
