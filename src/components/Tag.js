9282c5e18a2e07581de858323c9066ffb4163ec3
Patrik Nilsson <pn222en@student.lnu.se>
2018-07-23 23:18:56 +0200

    Adjust css tags

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -5,7 +5,7 @@ const Styling = styled.span`
   border: 1px solid #dee2e5;
   background-color: #dee2e5;
   border-radius: 5px;
-  padding: 2px 4px;
+  padding: 3px 7px;
   line-height: 13px;
   font-family: sans-serif;
   text-transform: uppercase;
