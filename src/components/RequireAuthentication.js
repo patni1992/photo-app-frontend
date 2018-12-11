de7892b948ab67f7d8bdee38621cbe954679433a
Patrik <patriknilsson1992@gmail.com>
2018-12-11 17:02:11 +0100

    fix back button not working after redirect to auth

--- src/components/RequireAuthentication.js
+++ src/components/RequireAuthentication.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from 'react-router'
 
 export default function(ComposedComponent) {
   class Authenticate extends Component {
+    state = {
+      redirect: false
+    }
     componentWillMount() {
       if (!this.props.isAuthenticated) {
-        this.props.history.push("/auth");
+        this.setState({
+          redirect: true
+        })
       }
     }
     render() {
+      if(this.state.redirect) {
+        return <Redirect to='/auth'/>;
+      }
       return <ComposedComponent {...this.props} />;
     }
   }
